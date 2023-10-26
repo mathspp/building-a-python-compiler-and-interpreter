ccb963a3d9f5a9c3b4defc7c5573d31ddc6fbbf6
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-10-26 10:36:59 +0100

    First tests for the tokenizer.

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,43 @@
+from tokenizer import Token, Tokenizer, TokenType
+
+
+def test_tokenizer_addition():
+    tokens = list(Tokenizer("3 + 5"))
+    assert tokens == [
+        Token(TokenType.INT, 3),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 5),
+        Token(TokenType.EOF),
+    ]
+
+
+def test_tokenizer_subtraction():
+    tokens = list(Tokenizer("3 - 6"))
+    assert tokens == [
+        Token(TokenType.INT, 3),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 6),
+        Token(TokenType.EOF),
+    ]
+
+
+def test_tokenizer_additions_and_subtractions():
+    tokens = list(Tokenizer("1 + 2 + 3 + 4 - 5 - 6 + 7 - 8"))
+    assert tokens == [
+        Token(TokenType.INT, 1),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 2),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 3),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 4),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 5),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 6),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 7),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 8),
+        Token(TokenType.EOF),
+    ]
