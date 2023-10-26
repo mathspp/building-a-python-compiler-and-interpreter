dc136b6a7877d7b8a16dad4e371ad6c546ee718a
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-10-26 10:38:14 +0100

    Add more tests for the tokenizer.

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,4 +1,19 @@
+import pytest
+
 from tokenizer import Token, Tokenizer, TokenType
+
+
+@pytest.mark.parametrize(
+    ["code", "token"],
+    [
+        ("+", Token(TokenType.PLUS)),
+        ("-", Token(TokenType.MINUS)),
+        ("3", Token(TokenType.INT, 3)),
+    ],
+)
+def test_tokenizer_recognises_each_token(code: str, token: Token):
+    tokens = list(Tokenizer(code))
+    assert tokens == [token, Token(TokenType.EOF)]
 
 
 def test_tokenizer_addition():
@@ -41,3 +56,30 @@ def test_tokenizer_additions_and_subtractions():
         Token(TokenType.INT, 8),
         Token(TokenType.EOF),
     ]
+
+
+def test_tokenizer_additions_and_subtractions_with_whitespace():
+    tokens = list(Tokenizer("     1+       2   +3+4-5  -   6 + 7  - 8        "))
+    assert tokens == [
+        Token(TokenType.INT, 1),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 2),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 3),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 4),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 5),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 6),
+        Token(TokenType.PLUS),
+        Token(TokenType.INT, 7),
+        Token(TokenType.MINUS),
+        Token(TokenType.INT, 8),
+        Token(TokenType.EOF),
+    ]
+
+
+def test_tokenizer_raises_error_on_garbage():
+    with pytest.raises(RuntimeError):
+        list(Tokenizer("$"))
