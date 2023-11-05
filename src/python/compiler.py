aa21da029e14042133c67abd119e77035f2d1131
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-11-05 18:39:34 +0000

    Compile sequences of additions and subtractions.

--- src/python/compiler.py
+++ src/python/compiler.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from enum import auto, StrEnum
 from typing import Any, Generator
 
-from .parser import BinOp
+from .parser import BinOp, Float, Int, TreeNode
 
 
 class BytecodeType(StrEnum):
@@ -22,24 +22,39 @@ class Bytecode:
         return f"{self.__class__.__name__}({self.type!r}, {self.value!r})"
 
 
+type BytecodeGenerator = Generator[Bytecode, None, None]
+
+
 class Compiler:
-    def __init__(self, tree: BinOp) -> None:
+    def __init__(self, tree: TreeNode) -> None:
         self.tree = tree
 
-    def compile(self) -> Generator[Bytecode, None, None]:
-        left = self.tree.left
-        yield Bytecode(BytecodeType.PUSH, left.value)
+    def compile(self) -> BytecodeGenerator:
+        yield from self._compile(self.tree)
 
-        right = self.tree.right
-        yield Bytecode(BytecodeType.PUSH, right.value)
+    def _compile(self, tree: TreeNode) -> BytecodeGenerator:
+        node_name = tree.__class__.__name__
+        compile_method = getattr(self, f"compile_{node_name}", None)
+        if compile_method is None:
+            raise RuntimeError(f"Can't compile {node_name}.")
+        yield from compile_method(tree)
 
-        yield Bytecode(BytecodeType.BINOP, self.tree.op)
+    def compile_BinOp(self, tree: BinOp) -> BytecodeGenerator:
+        yield from self._compile(tree.left)
+        yield from self._compile(tree.right)
+        yield Bytecode(BytecodeType.BINOP, tree.op)
+
+    def compile_Int(self, tree: Int) -> BytecodeGenerator:
+        yield Bytecode(BytecodeType.PUSH, tree.value)
+
+    def compile_Float(self, tree: Float) -> BytecodeGenerator:
+        yield Bytecode(BytecodeType.PUSH, tree.value)
 
 
 if __name__ == "__main__":
     from .tokenizer import Tokenizer
     from .parser import Parser
 
-    compiler = Compiler(Parser(list(Tokenizer("3 + 5"))).parse())
+    compiler = Compiler(Parser(list(Tokenizer("3 + 5 - 7 + 1.2 + 2.4 - 3.6"))).parse())
     for bc in compiler.compile():
         print(bc)
--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,5 +1,5 @@
 from python.compiler import Bytecode, BytecodeType, Compiler
-from python.parser import BinOp, Int
+from python.parser import BinOp, Float, Int
 
 
 def test_compile_addition():
@@ -26,5 +26,43 @@ def test_compile_subtraction():
     assert bytecode == [
         Bytecode(BytecodeType.PUSH, 5),
         Bytecode(BytecodeType.PUSH, 2),
+        Bytecode(BytecodeType.BINOP, "-"),
+    ]
+
+
+def test_compile_nested_additions_and_subtractions():
+    tree = BinOp(
+        "-",
+        BinOp(
+            "+",
+            BinOp(
+                "+",
+                BinOp(
+                    "-",
+                    BinOp(
+                        "+",
+                        Int(3),
+                        Int(5),
+                    ),
+                    Int(7),
+                ),
+                Float(1.2),
+            ),
+            Float(2.4),
+        ),
+        Float(3.6),
+    )
+    bytecode = list(Compiler(tree).compile())
+    assert bytecode == [
+        Bytecode(BytecodeType.PUSH, 3),
+        Bytecode(BytecodeType.PUSH, 5),
+        Bytecode(BytecodeType.BINOP, "+"),
+        Bytecode(BytecodeType.PUSH, 7),
+        Bytecode(BytecodeType.BINOP, "-"),
+        Bytecode(BytecodeType.PUSH, 1.2),
+        Bytecode(BytecodeType.BINOP, "+"),
+        Bytecode(BytecodeType.PUSH, 2.4),
+        Bytecode(BytecodeType.BINOP, "+"),
+        Bytecode(BytecodeType.PUSH, 3.6),
         Bytecode(BytecodeType.BINOP, "-"),
     ]
